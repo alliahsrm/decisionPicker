8dc4cd7427650c6cdc82084c872fe180e3785fcb
alliahsrm <alliahsarahmadrigal@gmail.com>
2023-05-26 19:29:23 +0800

    enhanced decision wheel
    changed from yes or no to user input their choices

--- Program.cs
+++ Program.cs
@@ -4,85 +4,58 @@ namespace decisionWheel
 {
     class Program
     {
-        static void Main()
+        static void Main(string[] args)
         {
             // Create a new instance of the Random class
             Random rand = new Random();
 
-            // Initialize variables to keep track of the number of "Yes" and "No" responses and the number of times the program has run
-            int yesCount = 0;
-            int noCount = 0;
-            int numIterations = 0;
+            // Initialize variables to keep track of the choices and the number of times the program has run
+            string[] choices;
+            int numChoices = 0;
 
-            // Create a while loop that will run as long as the number of iterations is less than 3
-            while (numIterations < 3)
+            while (true)
             {
-                // Display a menu to the user
-                Console.WriteLine("Welcome to Decision Wheel Roulette! ");
-                Console.WriteLine("1. Get a random decision for 3 times.");
+                Console.WriteLine("Welcome to Decision Wheel Roulette!");
+                Console.WriteLine("1. Insert how many choices.");
                 Console.WriteLine("2. Exit the program.");
 
-                // Get the user's choice
                 int userChoice = Convert.ToInt32(Console.ReadLine());
 
-                // Check the user's choice
-                if (userChoice == 1)
+                switch (userChoice)
                 {
-                    // Generate a random number between 0 and 1
-                    int randomNum = rand.Next(2);
+                    case 1:
+                        Console.Write("Please enter how many choices: ");
+                        numChoices = Convert.ToInt32(Console.ReadLine());
 
-                    // Check the random number
-                    if (randomNum == 0)
-                    {
-                        // Increment the "Yes" count and display "Yes"
-                        yesCount++;
-                        Console.WriteLine("Random decision: Yes");
-                    }
-                    else
-                    {
-                        // Increment the "No" count and display "No"
-                        noCount++;
-                        Console.WriteLine("Random decision: No");
-                    }
-
-                    // Increment the number of iterations
-                    numIterations++;
-
-                    // Check if the program has run three times
-                    if (numIterations == 3)
-                    {
-                        // Display the final decision based on the count of "Yes" and "No" responses
-                        Console.WriteLine("Press enter to get the final decision:");
-                        Console.ReadLine();
-
-                        if (yesCount > noCount)
+                        if (numChoices < 2)
                         {
-                            Console.WriteLine("Final decision: Yes");
-                            Console.WriteLine("Thank you for using Decision Wheel! ");
+                            Console.WriteLine("\nError: Invalid input. You need to enter at least 2 choices.\n");
+                            break;
                         }
-                        else if (noCount > yesCount)
-                        {
-                            Console.WriteLine("Final decision: No");
-                            Console.WriteLine("Thank you for using Decision Wheel! ");
-                        }
-                        else
-                        {
-                            Console.WriteLine("Final decision: Undecided");
-                            Console.WriteLine("Thank you for using Decision Wheel! ");
-                        }
-                    }
-                }
-                else if (userChoice == 2)
-                {
-                    // Exit the program if the user selects option 2
-                    Console.WriteLine("Exiting the program... ");
-                    break;
-                }
-                else
-                {
-                    // Display an error message if the user enters an invalid input
-                    Console.WriteLine("Error: invalid input");
 
+                        choices = new string[numChoices];
+
+                        for (int i = 0; i < numChoices; i++)
+                        {
+                            Console.Write($"Please enter choice {i + 1}: ");
+                            choices[i] = Console.ReadLine();
+                        }
+
+                        int finalIndex = rand.Next(numChoices);
+                        string finalChoice = choices[finalIndex];
+
+                        Console.WriteLine("\nFinal decision:");
+                        Console.WriteLine(finalChoice);
+                        Console.WriteLine("Thank you for using Decision Wheel!");
+                        return;
+
+                    case 2:
+                        Console.WriteLine("Exiting the program...");
+                        return;
+
+                    default:
+                        Console.WriteLine("Error: Invalid input. Please choose option 1 or 2.");
+                        break;
                 }
             }
         }
