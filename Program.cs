8da9ec4f9b3a38d303a5586db29c6adfebe58735
alliahsrm <alliahsarahmadrigal@gmail.com>
2023-06-10 08:30:01 +0800

    updated program
    added list and method

--- Program.cs
+++ Program.cs
@@ -1,63 +1,112 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace decisionWheel
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            // Create a new instance of the Random class
-            Random rand = new Random();
+        static List<string> history = new List<string>();
 
-            // Initialize variables to keep track of the choices and the number of times the program has run
-            string[] choices;
-            int numChoices = 0;
+        static void Main()
+        {
+            Random rand = new Random();
+            List<string> choices = new List<string>();
 
             while (true)
             {
-                Console.WriteLine("Welcome to Decision Wheel Roulette!");
-                Console.WriteLine("1. Insert how many choices.");
-                Console.WriteLine("2. Exit the program.");
+                Console.WriteLine("Welcome to the Decision Wheel Roulette!\n");
+                Console.WriteLine("This program will help you decide on things you can't choose for yourself.");
+                Console.WriteLine("Just pick a number below to navigate this program...\n");
+                Console.WriteLine("1. Insert how many choices");
+                Console.WriteLine("2. Show history");
+                Console.WriteLine("3. Exit the program");
 
-                int userChoice = Convert.ToInt32(Console.ReadLine());
+                int userInput = Convert.ToInt32(Console.ReadLine());
 
-                switch (userChoice)
+                switch (userInput)
                 {
                     case 1:
-                        Console.Write("Please enter how many choices: ");
-                        numChoices = Convert.ToInt32(Console.ReadLine());
-
-                        if (numChoices < 2)
-                        {
-                            Console.WriteLine("\nError: Invalid input. You need to enter at least 2 choices.\n");
-                            break;
-                        }
-
-                        choices = new string[numChoices];
-
-                        for (int i = 0; i < numChoices; i++)
-                        {
-                            Console.Write($"Please enter choice {i + 1}: ");
-                            choices[i] = Console.ReadLine();
-                        }
-
-                        int finalIndex = rand.Next(numChoices);
-                        string finalChoice = choices[finalIndex];
-
-                        Console.WriteLine("\nFinal decision:");
-                        Console.WriteLine(finalChoice);
-                        Console.WriteLine("Thank you for using Decision Wheel!");
-                        return;
-
+                        InsertChoices(rand, choices);
+                        break;
                     case 2:
-                        Console.WriteLine("Exiting the program...");
+                        ShowHistory();
+                        break;
+                    case 3:
+                        Console.WriteLine("\nExiting the program...");
                         return;
-
                     default:
-                        Console.WriteLine("Error: Invalid input. Please choose option 1 or 2.");
+                        Console.WriteLine("\nError: Invalid input.");
                         break;
                 }
+
+                Console.WriteLine("\nPress '4' to use the program again or '5' to exit.");
+
+                string exitInput = Console.ReadLine()!;
+
+                if (exitInput == "5")
+                {
+                    Console.WriteLine("\nExiting the program...");
+                    return;
+                }
+
+                while (exitInput != "4" && exitInput != "5")
+                {
+                    Console.WriteLine("\nError: Invalid input.");
+                    Console.WriteLine("Press '4' to use the program again or '5' to exit.");
+
+                    exitInput = Console.ReadLine()!;
+
+                    if (exitInput == "5")
+                    {
+                        Console.WriteLine("\nExiting the program...");
+                        return;
+                    }
+                }
+
+                Console.WriteLine();
             }
+        }
+
+        static void InsertChoices(Random rand, List<string> choices)
+        {
+            Console.WriteLine("\nPlease enter how many choices:");
+            int numChoices = Convert.ToInt32(Console.ReadLine());
+
+            choices.Clear();
+
+            for (int i = 1; i <= numChoices; i++)
+            {
+                Console.WriteLine($"Please enter choice {i}:");
+                choices.Add(Console.ReadLine()!);
+            }
+
+            string finalChoice = "";
+
+            if (choices.Count > 0)
+            {
+                int randomIndex = rand.Next(choices.Count);
+                finalChoice = choices[randomIndex].ToUpper();
+            }
+
+            Console.WriteLine($"\nFinal decision: {finalChoice}");
+            Console.WriteLine("Thank you for using Decision Wheel!");
+
+            history.Add(finalChoice);
+
+            Console.WriteLine();
+        }
+
+        static void ShowHistory()
+        {
+            Console.WriteLine("\nDecision history:");
+
+            if (history.Count == 0)
+                Console.WriteLine("No history available.");
+            else
+                foreach (string choice in history)
+                    Console.WriteLine(choice);
+
+            Console.WriteLine();
         }
     }
 }
