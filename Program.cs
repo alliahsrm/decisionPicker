2abd0c1e958336b69c060c2866dc857c1e17a0ed
alliahsrm <alliahsarahmadrigal@gmail.com>
2023-07-29 08:17:44 +0800

    added class

--- Program.cs
+++ Program.cs
@@ -1,24 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace decisionWheel
+namespace decisionPicker
 {
     class Program
     {
-        static List<string> history = new List<string>();
-
         static void Main()
         {
-            Random rand = new Random();
-            List<string> choices = new List<string>();
+            DecisionManager decisionPicker = new DecisionManager();
 
             while (true)
             {
-                Console.WriteLine("Welcome to the Decision Wheel Roulette!\n");
+                Console.WriteLine("Welcome to the Decision Picker!\n");
                 Console.WriteLine("This program will help you decide on things you can't choose for yourself.");
                 Console.WriteLine("Just pick a number below to navigate this program...\n");
                 Console.WriteLine("1. Insert how many choices");
-                Console.WriteLine("2. Show history");
+                Console.WriteLine("2. Show decision history");
                 Console.WriteLine("3. Exit the program");
 
                 int userInput = Convert.ToInt32(Console.ReadLine());
@@ -26,10 +23,11 @@ namespace decisionWheel
                 switch (userInput)
                 {
                     case 1:
-                        InsertChoices(rand, choices);
+                        List<string> choices = GetChoices();
+                        decisionPicker.AddChoices(choices);
                         break;
                     case 2:
-                        ShowHistory();
+                        ShowDecisionHistory(decisionPicker);
                         break;
                     case 3:
                         Console.WriteLine("\nExiting the program...");
@@ -37,6 +35,16 @@ namespace decisionWheel
                     default:
                         Console.WriteLine("\nError: Invalid input.");
                         break;
+                }
+
+                if (userInput == 1)
+                {
+                    string pickedChoice = decisionPicker.MakeDecision();
+                    if (!string.IsNullOrEmpty(pickedChoice))
+                    {
+                        Console.WriteLine($"\nFINAL DECISION: {pickedChoice}");
+                        Console.WriteLine("Thank you for using Decision Picker!");
+                    }
                 }
 
                 Console.WriteLine("\nPress '4' to use the program again or '5' to exit.");
@@ -67,12 +75,12 @@ namespace decisionWheel
             }
         }
 
-        static void InsertChoices(Random rand, List<string> choices)
+        static List<string> GetChoices()
         {
             Console.WriteLine("\nPlease enter how many choices:");
             int numChoices = Convert.ToInt32(Console.ReadLine());
 
-            choices.Clear();
+            List<string> choices = new List<string>();
 
             for (int i = 1; i <= numChoices; i++)
             {
@@ -80,24 +88,12 @@ namespace decisionWheel
                 choices.Add(Console.ReadLine()!);
             }
 
-            string finalChoice = "";
-
-            if (choices.Count > 0)
-            {
-                int randomIndex = rand.Next(choices.Count);
-                finalChoice = choices[randomIndex].ToUpper();
-            }
-
-            Console.WriteLine($"\nFinal decision: {finalChoice}");
-            Console.WriteLine("Thank you for using Decision Wheel!");
-
-            history.Add(finalChoice);
-
-            Console.WriteLine();
+            return choices;
         }
 
-        static void ShowHistory()
+        static void ShowDecisionHistory(DecisionManager decisionPicker)
         {
+            List<string> history = decisionPicker.GetDecisionHistory();
             Console.WriteLine("\nDecision history:");
 
             if (history.Count == 0)
