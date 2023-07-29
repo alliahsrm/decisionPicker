b1f7f19044aff1f2ad8264da32e686faa8c3bab5
Alliah Sarah <130113362+alliahsrm@users.noreply.github.com>
2023-07-29 08:18:49 +0800

    Updated class

--- DecisionManager.cs
+++ DecisionManager.cs
@@ -1,12 +1,49 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace decisionWheel
+namespace decisionPicker
 {
-    internal class DecisionManager
+    public class DecisionManager
     {
+        private Random rand;
+        private List<string> choices;
+        private List<string> history;
+
+        public DecisionManager()
+        {
+            rand = new Random();
+            choices = new List<string>();
+            history = new List<string>();
+        }
+
+        public List<string> Choices
+        {
+            get { return choices; }
+        }
+
+        public void AddChoices(List<string> newChoices)
+        {
+            choices.Clear();
+            choices.AddRange(newChoices);
+        }
+
+        public string MakeDecision()
+        {
+            string finalChoice = "";
+
+            if (choices.Count > 0)
+            {
+                int randomIndex = rand.Next(choices.Count);
+                finalChoice = choices[randomIndex].ToUpper();
+                history.Add($"{DateTime.Now}: {finalChoice}");
+            }
+
+            return finalChoice;
+        }
+
+        public List<string> GetDecisionHistory()
+        {
+            return history;
+        }
     }
 }
